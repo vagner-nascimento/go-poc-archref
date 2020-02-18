679814a0c8846d9a530e5a212f0dce185048024e
VAGNER NASCIMENTO <x309357@gruposantander.com>
2020-02-17 22:07:54 -0300

    Fix - Finished architecture's organization for now

--- infra/repository/customerubscriber.go
+++ infra/repository/customerubscriber.go
@@ -10,18 +10,17 @@ func newCustomerSubscriber() data.AmqSubscriber {
 	consumerMessageHandler := func(data []byte) {
 		c, err := app.BuildCustomerFromBytes(data)
 		if err == nil {
+			err = app.AddCustomer(&c, &customerRepository{})
+			if err != nil {
+				infra.LogError("error on create a customer", err)
+			} else {
+				infra.LogInfo("customer created")
 
+				u := app.BuildUserFromCustomer(c)
+				go app.AddUser(&u, &userRepository{})
+			}
 		} else {
 			infra.LogError("error on convert message's body into a Customer", err)
-		}
-		err = app.AddCustomer(&c, &customerRepository{})
-		if err != nil {
-			infra.LogError("error on create a customer", err)
-		} else {
-			infra.LogInfo("customer created")
-
-			u := app.BuildUserFromCustomer(c)
-			go app.AddUser(&u, &userRepository{})
 		}
 	}
 
